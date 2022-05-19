6a713a4d901fea1d5a650dac275adaf52cede163
eiki tsutou <eikitsutou0920@gmail.com>
2022-05-19 06:07:51 +0000

    microposts

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  include SessionsHelper
+  
+  include Pagy::Backend
+  
+  
+  
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+    @count_favorite = user.likes.count
+  end
+
+end
